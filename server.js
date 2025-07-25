deee587591ae920eb4448c5e2c97c3fdeb92be3c
RAJ KUMAR HEMBRAM <145889010+rajkumar0301@users.noreply.github.com>
2025-07-25 16:32:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -130,6 +130,9 @@ function authenticateToken(req, res, next) {
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname, '../public/index.html'));
 // });
+app.get('/', (req, res) => {
+  res.send('SmartCity backend is live 🚀');
+});
 
 // Start server
 app.listen(PORT, () => {
